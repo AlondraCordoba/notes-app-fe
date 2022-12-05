c27ec5283e06d6894c296afd7f03f0e8803a8e5c
AlondraCordoba <alondracordoba0720 @gmail.com>
2022-12-05 11:22:52 -0600

    feat: last changes

--- components/DashboardComponent/DashboardComponent.js
+++ components/DashboardComponent/DashboardComponent.js
@@ -1,12 +1,55 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { Text, View, StyleSheet, SafeAreaView, TouchableOpacity} from 'react-native'
+import React, { Component, useState, useEffect } from 'react'
+import { firebase } from "../../config/firebase";
+
 
 const Dashboard = (props) => {
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).get()
+    .then((snapshot) => {
+      if(snapshot.exists){
+        setName(snapshot.data())
+      }
+      else {
+        console.log('User dose not exist');
+      }
+    })
+  }, [])
+
     return (
-      <View>
-        <Text>DashboardComponent</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <Text style={{fontSize:20, fontWeight:'bold'}}>
+          Hello, {name.firstName}
+        </Text>
+        <TouchableOpacity
+        onPress={() => firebase.auth().signOut()}
+        styles={styles.button}
+        >
+        <Text style={{fontSize:22, fontWeight:'bold'}}>
+          Sign out
+        </Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     )
 }
 
 export default Dashboard;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop:100,
+  },
+  button:{
+    marginTop:50,
+    height:70,
+    width:250,
+    backgroundColor:'#026efd',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius:50,
+  }
+})
--- components/SignUpComponent/SignupComponent.js
+++ components/SignUpComponent/SignupComponent.js
@@ -1,12 +1,137 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+import React, { Component, useState, useEffect } from "react";
+import { useNavigation } from '@react-navigation/native'
+import { firebase } from "../../config/firebase";
 
 const SignUp = (props) => {
-    return (
-      <View>
-        <Text>SignUpComponent</Text>
+  const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const registerUser = async (email, password, firstName, lastName) => {
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        firebase
+          .auth()
+          .currentUser.sendEmailVerification({
+            handleCodeInApp: true,
+            url: "https://notes-app-1c6cb.firebaseapp.com",
+          })
+          .then(() => {
+            alert("Verification email sent successfully");
+          })
+          .catch((error) => {
+            alert(error.message);
+          })
+          .then(() => {
+            firebase
+              .firestore()
+              .collection("users")
+              .doc(firebase.auth().currentUser.uid)
+              .set({
+                firstName,
+                lastName,
+                email,
+              });
+          })
+          .catch((error) => {
+            alert(error.message);
+          });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontWeight: "bold", fontSize: 23 }}>SignUp</Text>
+      <View style={{ marginTop: 40 }}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="First Name"
+          onChangeText={(firstName) => setFirstName(firstName)}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Last Name"
+          onChangeText={(lastName) => setLastName(lastName)}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Email"
+          onChangeText={(email) => setEmail(email)}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Password"
+          onChangeText={(password) => setPassword(password)}
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={true}
+        />
       </View>
-    )
-}
+
+      <TouchableOpacity
+        onPress={() => registerUser(email, password, lastName, firstName)}
+        style={styles.button}
+      >
+        <Text style={{ fontWeight: "bold", fontSize: 22 }}>Sign up</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Login")}
+        style={{ marginTop: 20 }}
+      >
+        <Text style={{ fontWeight: "bold", fontSize: 16 }}>
+          Do you already have an account? Login Now
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default SignUp;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 100,
+  },
+  textInput: {
+    paddingTop: 20,
+    paddingBottom: 10,
+    width: 400,
+    fontSize: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#000",
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  button: {
+    marginTop: 50,
+    height: 70,
+    width: 250,
+    backgroundColor: "#026efd",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+  },
+});
